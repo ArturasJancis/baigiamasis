69c9e90fabec22d4e2b99e03e2b4bef4be4a1f85
ArturasJancis <arturas.jancis@gmail.com>
2023-09-08 17:06:18 +0300

    fix image

--- src/App.js
+++ src/App.js
@@ -14,13 +14,13 @@ function App() {
       name: "Fluffy",
       type: "Cat",
       age: 3,
-      image: "https://example.com/fluffy.jpg",
+      image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.IadWjX7Z2rp2w7Td5k-LAwHaFA%26pid%3DApi&f=1&ipt=520c70252e684959ed5e0398330d9ab58c92c678bca2e5f855c818539bbc20b8&ipo=images",
     },
     {
       name: "Buddy",
       type: "Dog",
       age: 2,
-      image: "https://example.com/buddy.jpg",
+      image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.49CH_m8xcY3b6stGkMkG2AHaEO%26pid%3DApi&f=1&ipt=a9c338652443d4ab455ca3870b36431483b3645d0e7c5ae897a8559e240af67b&ipo=images",
     },
   ]);
 
