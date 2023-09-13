06809cf6738139f641047c248437af22e76d3e45
ArturasJancis <arturas.jancis@gmail.com>
2023-09-13 15:49:14 +0300

    dots

--- src/pages/SingleAnimalPage.js
+++ src/pages/SingleAnimalPage.js
@@ -11,67 +11,67 @@ const SingleAnimalPage = ({ animalData, favoritesCount, setAnimalData }) => {
   const { addComment, deleteComment } = useCommentContext(); 
   const navigate = useNavigate(); 
 
-  // useEffect to load the current animal data when the component mounts.
+  // useEffect to load the current animal data when the component mounts
   useEffect(() => {
     if (animalData && animalId) {
-      // Find the animal in the animalData array that matches the animalId.
+      // Find the animal in the animalData array that matches the animalId
       const foundAnimal = animalData.find((animal) => animal.name === animalId);
 
       if (!foundAnimal.comments) {
-        // Initialize comments if they don't exist.
+        // Initialize comments if they don't exist
         foundAnimal.comments = [];
       }
 
-      // Set the currentAnimal state with the found animal data.
+      // Set the currentAnimal state with the found animal data
       setCurrentAnimal(foundAnimal);
     }
   }, [animalData, animalId, favoritesCount]);
 
-  // Function to add a comment to the current animal.
+  // Function to add a comment to the current animal
   const addCommentToCurrentAnimal = (comment) => {
     if (currentAnimal) {
-      // Create an updated animal object with the new comment.
+      // Create an updated animal object with the new comment
       const updatedAnimal = {
         ...currentAnimal,
         comments: [...currentAnimal.comments, comment],
       };
-      // Update the animalData array with the updated animal.
+      // Update the animalData array with the updated animal
       const updatedAnimalData = animalData.map((animal) =>
         animal.name === currentAnimal.name ? updatedAnimal : animal
       );
-      setAnimalData(updatedAnimalData); // Update the animal data in the parent component.
+      setAnimalData(updatedAnimalData); // Update the animal data in the parent component
     }
   };
 
-  // Function to handle adding a comment.
+  // Function to handle adding a comment
   const handleAddComment = () => {
     if (newComment.trim() !== "") {
-      addCommentToCurrentAnimal(newComment); // Add the comment to the current animal.
-      addComment(newComment); // Add the comment to the global comment context.
-      setNewComment(""); // Clear the input field.
+      addCommentToCurrentAnimal(newComment); // Add the comment to the current animal
+      addComment(newComment); // Add the comment to the global comment context
+      setNewComment(""); // Clear the input field
     }
   };
 
-  // Function to handle deleting a comment.
+  // Function to handle deleting a comment
   const handleDeleteComment = (index) => {
     if (currentAnimal) {
-      // Create an updated comments array without the deleted comment.
+      // Create an updated comments array without the deleted comment
       const updatedComments = [...currentAnimal.comments];
       updatedComments.splice(index, 1);
-      // Create an updated animal object with the updated comments.
+      // Create an updated animal object with the updated comments
       const updatedAnimal = { ...currentAnimal, comments: updatedComments };
-      // Update the animalData array with the updated animal.
+      // Update the animalData array with the updated animal
       const updatedAnimalData = animalData.map((animal) =>
         animal.name === currentAnimal.name ? updatedAnimal : animal
       );
-      setAnimalData(updatedAnimalData); // Update the animal data in the parent component.
+      setAnimalData(updatedAnimalData); // Update the animal data in the parent component
     }
-    deleteComment(index); // Delete the comment from the global comment context.
+    deleteComment(index); // Delete the comment from the global comment context
   };
 
-  // Function to handle logging out and navigating back to the homepage.
+  // Function to handle logging out and navigating back to the homepage
   const handleLogout = () => {
-    navigate("/"); // Navigate to the homepage.
+    navigate("/"); 
   };
 
   return (
